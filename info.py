40c6b7987288caa8ffcc8a800eeb2b441ff7afa5
Prajwal-027 <gamerprajwalxp@gmail.com>
2025-05-18 09:22:43 +0530

    Update info.py

--- info.py
+++ info.py
@@ -7,10 +7,10 @@
 import os
 from os import environ
 
-API_ID       = int(environ.get("API_ID", ""))
-API_HASH     = environ.get("API_HASH", "")
+API_ID       = int(environ.get("API_ID", "28690893"))
+API_HASH     = environ.get("API_HASH", "c214f988aa1ac0b998ace0b7cd0e215f")
 BOT_TOKEN    = environ.get("BOT_TOKEN", "")
 DATABASE_URI = environ.get("DATABASE_URI", "")
-LOG_CHANNEL  = int(environ.get("LOG_CHANNEL", ""))
-ADMIN        = int(environ.get("ADMIN", ""))
-CHANNEL      = environ.get("CHANNEL", "@VJ_Botz")
+LOG_CHANNEL  = int(environ.get("LOG_CHANNEL", "-1002480514355"))
+ADMIN        = int(environ.get("ADMIN", "1454524346"))
+CHANNEL      = environ.get("CHANNEL", "https://t.me/+RL6sEUJsWHU0ZDc1")
